0c54131ef225482fa8bbf5cc9e20f9b04a5e108c
Hicham Kaber <hichamkaber9@gmail.com>
2026-05-10 13:22:02 +0100

    start

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std ;
+
+// definir la classe Rectangle 
+class Rectangle  {
+
+public :
+float Longueur ;
+float Largeur ;
+//definir les constructeurs 
+Rectangle(float L, float l ){
+    Longueur=L ;
+    Largeur=l ;
+}
+// definir methode 
+float surface(){
+    return Largeur*Largeur ;
+}
+
+
+};
+int main(){
+    Rectangle r(5,6);
+    cout << "la surface de la Rectangle est :" ;
+cout<< r.surface();
+return 0 ;
+}
